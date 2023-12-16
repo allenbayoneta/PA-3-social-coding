e73aa59d690934c2062d7bda3982935091ef445c
allenbayoneta <ajpbayoneta@gmail.com>
2023-12-16 15:33:39 +0000

    adding ip api

---
+++ api.py
@@ -0,0 +1,27 @@
+import requests
+
+
+def get_ip():
+    response = requests.get('https://api.ipify.org?format=json').json()
+    return response['ip']
+
+
+def get_location():
+    ip_address = get_ip()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    location_data = {
+        "IPv4 Address": ip_address,
+        "City": response.get("city"),
+        "Region": response.get("region"),
+        "Country": response.get("country_name"),
+        "ASN": response.get("asn"),
+        "Provider (ISP)" : response.get("org"),
+        "Latitude / Longitude": str(response.get("latitude")) + ", " + str(response.get("longitude")),
+    }
+    return location_data
+
+ip_address = get_ip()
+print(ip_address)
+
+location_data = get_location()
+print(location_data)
---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, render_template
+from api import get_location
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_location', methods=['GET'])
+def get_location_route():
+    location_data = get_location()
+    return jsonify(location_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
